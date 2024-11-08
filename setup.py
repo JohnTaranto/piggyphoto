947c91541d0c76b6b7885fcec1646dd5643e68b6
JohnTaranto <48106466+JohnTaranto@users.noreply.github.com>
2024-11-08 20:14:01 +1100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="piggyphoto",
+    version="0.1",
+    description="Python wrapper for libgphoto2",
+    author="alexdu",
+    packages=["piggyphoto"],
+)
